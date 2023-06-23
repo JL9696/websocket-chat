3e4d918bb51032c474889df72e23530d65a19d98
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-06-23 17:15:27 +0200

    add socket.io, add function

--- client/app.js
+++ client/app.js
@@ -1,3 +1,7 @@
+const socket = io();
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
 const loginForm = document.querySelector('#welcome-form');
 const messagesSection = document.querySelector('#messages-section');
 const messagesList = document.querySelector('#messages-list');
@@ -9,7 +13,7 @@ let userName = '';
 
 const login = (e) => {
   e.preventDefault();
-  if(userNameInput.value.length > 0) {
+  if (userNameInput.value.length > 0) {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
@@ -23,9 +27,9 @@ function addMessage(author, content) {
   const message = document.createElement('li');
   message.classList.add('message');
   message.classList.add('message--received');
-  if(author === userName) message.classList.add('message--self');
+  if (author === userName) message.classList.add('message--self');
   message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
     <div class="message__content">
       ${content}
     </div>
@@ -33,15 +37,17 @@ function addMessage(author, content) {
   messagesList.appendChild(message);
 }
 
-const sendMessage = (e) => {
+function sendMessage(e) {
   e.preventDefault();
-  if (messageContentInput.value.length > 0) {
-    addMessage(userName, messageContentInput.value);
-    messageContentInput.value = "";
-} else {
-    alert('Please enter a message');
-}
-}
+  let messageContent = messageContentInput.value;
+  if (!messageContent.length) {
+    alert('Please enter a message');  
+  } else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent });
+    messageContentInput.value = '';
+  }
+};
 
 loginForm.addEventListener('submit', (e) => login(e));
 addMessageForm.addEventListener('submit', (e) => sendMessage(e));
